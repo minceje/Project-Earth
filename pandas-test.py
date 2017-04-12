2b272210461bb0da7638dc3fccf4dc4c5d41e3f0
wesleymerrick <wesleymerrick@gmail.com>
2017-04-12 17:33:11 -0400

    Finished concept

--- pandas-test.py
+++ pandas-test.py
@@ -14,12 +14,12 @@ avg_list = avg_list[0:200]
 sl_list = [int(i * 100) for i in sl_list]
 avg_list = [int(j * 100) for j in avg_list]
 
-sl_arr = np.array(sl_list).reshape(-1, 1)
 avg_arr = np.array(avg_list).reshape(-1, 1)
+sl_arr = np.array(sl_list).ravel()
 
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(avg_arr, sl_arr)
 
-pred = knn.predict(61)
-print("Predicted sea level given an index of 61 (0.61 * 100): ", pred)
-print("Expected output: -409 (-4.09 * 100 for ALA 2016 Season 3)")
+pred = float(knn.predict(61) / 100)
+print("Predicted sea level given an index of 0.61: ", pred)
+print("Expected output: -4.09, SL ALA 2016 Season 3")
