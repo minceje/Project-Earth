43f4977ee095a9736bf1da92a66ea93e48333e66
wesleymerrick <wesleymerrick@gmail.com>
2017-04-12 17:25:15 -0400

    Fixed it, except for 1d array nonsense

--- pandas-test.py
+++ pandas-test.py
@@ -4,12 +4,7 @@ from sklearn.neighbors import KNeighborsClassifier
 
 sl = pd.read_csv('5yrSeaLevel.csv')
 avg = pd.read_csv('5yrAvg.csv')
-# print(sl.tail())
-# print(avg.tail())
-# print(sl[0:10])
-# ca = np.array(df['t90 USC 5'].tolist())
-# print(ca)
-# print(type(ca))
+
 sl_list = sl[sl.columns[0]].tolist()
 avg_list = avg[avg.columns[0]].tolist()
 
@@ -18,14 +13,13 @@ avg_list = avg_list[0:200]
 
 sl_list = [int(i * 100) for i in sl_list]
 avg_list = [int(j * 100) for j in avg_list]
-print(sl_list)
 
 sl_arr = np.array(sl_list).reshape(-1, 1)
-avg_arr = np.ravel(avg_list)
+avg_arr = np.array(avg_list).reshape(-1, 1)
 
-# knn = KNeighborsClassifier(n_neighbors=3)
-# knn.fit(avg_list, sl_list)
-#
-# pred = knn.predict(70)
-# print("Predicted sea level given an index of 0.79: ", pred)
-# print("Expected output: -0.60  (USA 2016 Season 3)")
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(avg_arr, sl_arr)
+
+pred = knn.predict(61)
+print("Predicted sea level given an index of 61 (0.61 * 100): ", pred)
+print("Expected output: -409 (-4.09 * 100 for ALA 2016 Season 3)")
