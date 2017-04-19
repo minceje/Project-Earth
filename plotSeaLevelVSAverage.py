6728cea9be4f464be86bcfc3137a1658f8ec3c62
kesslerk <kskessler@smcm.edu>
2017-04-19 13:59:02 -0400

    put sea level on y axis

--- plotSeaLevelVSAverage.py
+++ plotSeaLevelVSAverage.py
@@ -18,10 +18,11 @@ for j in range(4):
         sl_list = sl[sl.columns[i+2]].tolist()
         avg_list = avg[avg.columns[i+2]].tolist()
         
-        plt.title(str(list(sl)[i+2]) + " vs AVG " +  str(list(avg)[i+2]) + " Season " + str(j+1))
-        plt.xlabel(str(list(sl)[i+2]))
-        plt.ylabel("AVG " + str(list(avg)[i+2]))
-        plt.scatter(sl_list[j::4], avg_list[j::4], color=colors[i])
+        plt.title("AVG " + str(list(avg)[i+2])+ " vs " + str(list(sl)[i+2]) + " Season " + str(j+1))
+        plt.xlabel("AVG " + str(list(avg)[i+2]))
+        plt.ylabel(str(list(sl)[i+2]))
+        
+        plt.scatter(avg_list[j::4], sl_list[j::4], color=colors[i])
         plt.show()
 
 
