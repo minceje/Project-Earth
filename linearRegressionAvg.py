e3f1b095d9e1235b3f9bd694e928f507f707eac6
kesslerk <kskessler@smcm.edu>
2017-04-26 13:58:05 -0400

    Add comments to code

--- linearRegressionAvg.py
+++ linearRegressionAvg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 21 13:52:33 2017
+Created on Wed Apr 26 13:51:46 2017
 
 @author: katiekessler
 """
@@ -12,15 +12,15 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 
 sl = pd.read_csv('CSV Data Set Files for Code/ACI - SL.csv')
-rx5 = pd.read_csv('CSV Data Set Files for Code/ACI - rx5.csv')
+avg = pd.read_csv('CSV Data Set Files for Code/ACI - avg.csv')
 
 for j in range(4): #go through every season
     for i in range(13): #go through every region
         sl_list = sl[sl.columns[i+2]].tolist() #get sea level for a certain region
-        rx5_list = rx5[rx5.columns[i+2]].tolist() #get rx5 for the same region
+        avg_list = avg[avg.columns[i+2]].tolist() #get avg for the same region
         
         xo1 = sl_list[j::4] #split the data by season j
-        yo1 = rx5_list[j::4] #split the data by season j
+        yo1 = avg_list[j::4] #split the data by season j
 
         #linear regression
         x = np.array(xo1).reshape(-1,1) #shape the data from linear regression
@@ -36,23 +36,13 @@ for j in range(4): #go through every season
 
         regr.fit(x_train, y_train)
 
-        print("R-squared: " + str(list(rx5)[i+2]) + " " + str(regr.score(x_test,y_test))) #print r-squared value
+        print("R-squared: avg " + str(list(avg)[i+2]) + " " + str(regr.score(x_test,y_test))) #print r-squared value
 
         
-        plt.title(str(list(rx5)[i+2])+ " vs " + str(list(sl)[i+2]) + " Season " + str(j+1))
-        plt.xlabel(str(list(rx5)[i+2]))
+        plt.title("avg " + str(list(avg)[i+2])+ " vs " + str(list(sl)[i+2]) + " Season " + str(j+1))
+        plt.xlabel("avg " + str(list(avg)[i+2]))
         plt.ylabel(str(list(sl)[i+2]))
         
-        plt.scatter(rx5_list[j::4], sl_list[j::4], color='black') #plot data and linear regression line
+        plt.scatter(avg_list[j::4], sl_list[j::4], color='black') #plot data and linear regression line
         plt.plot(x_test, regr.predict(x_test), color='blue', linewidth=3)
         plt.show()
-
-
-
-
-
-
-
-    
-
-
